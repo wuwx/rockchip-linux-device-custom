4abcd93c6342e0c926e3043ee675521e82530bf8
Binyuan Lan <lby@rock-chips.com>
2016-12-15 11:18:41 +0800

    add some need fix imgs
    Change-Id: If33fe20be77b8d2db3b5a1c00450e79ecd01769b
    Signed-off-by: Binyuan Lan <lby@rock-chips.com>

--- mkfirmware.sh
+++ mkfirmware.sh
@@ -12,6 +12,10 @@ cp $(pwd)/buildroot/output/images/rootfs.ext4 $IMAGE_OUT_PATH/rootfs.img
 
 cp $KERNEL_PATH/kernel.img $IMAGE_OUT_PATH
 cp $KERNEL_PATH/resource.img $IMAGE_OUT_PATH
-cp $UBOOT_PATH/*MiniLoaderAll_*.bin $IMAGE_OUT_PATH/MiniLoaderAll.bin
-cp $UBOOT_PATH/uboot.img $IMAGE_OUT_PATH
+#cp $UBOOT_PATH/*MiniLoaderAll_*.bin $IMAGE_OUT_PATH/MiniLoaderAll.bin
+#cp $UBOOT_PATH/uboot.img $IMAGE_OUT_PATH
+cp $(pwd)/device/rockchip/rk3399/rockimg/RK3399MiniLoaderAll_V1.05.bin $IMAGE_OUT_PATH/
+cp $(pwd)/device/rockchip/rk3399/rockimg/parameter.txt $IMAGE_OUT_PATH/
+cp $(pwd)/device/rockchip/rk3399/rockimg/uboot.img $IMAGE_OUT_PATH/
+cp $(pwd)/device/rockchip/rk3399/rockimg/trust.img $IMAGE_OUT_PATH/
 echo 'Image: image in rockimg is ready'
