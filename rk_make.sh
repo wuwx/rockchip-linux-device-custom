b7073d8ae734583e6a9318f3f6c3bf5abf864e3e
Binyuan Lan <lby@rock-chips.com>
2016-12-30 17:18:19 +0800

    add Carmachine main window and gpu libs
    Change-Id: I691e888038e5d91a55300975a956d26deb4c3dee
    Signed-off-by: Binyuan Lan <lby@rock-chips.com>

--- rk_make.sh
+++ rk_make.sh
@@ -3,4 +3,12 @@
 TOP_DIR=$(pwd)
 BUILDROOT_TARGET_PATH=$(pwd)/../../../buildroot/output/target/
 
+#gpu
+rm $BUILDROOT_TARGET_PATH/usr/lib/libwayland-egl.so*
+rm $BUILDROOT_TARGET_PATH/usr/lib/libgbm.so*
+rm $BUILDROOT_TARGET_PATH/usr/lib/libEGL.so*
+rm $BUILDROOT_TARGET_PATH/usr/lib/libGLESv*
+cp -d lib/gpu/* $BUILDROOT_TARGET_PATH/usr/lib/
+
+cp bin/Carmachine $BUILDROOT_TARGET_PATH/usr/bin/
 cp S50rk3399init $BUILDROOT_TARGET_PATH/etc/init.d/
