e46de21d9eb2c47fa3c54a724dc158a8fef78e9d
ldq <david.li@rock-chips.com>
2017-01-04 11:53:38 +0800

    add alsa config file for codec of rt5651 on 3399
    Change-Id: I2d15234f791ea8f1280424838ec1043a4650ff0e

--- rk_make.sh
+++ rk_make.sh
@@ -16,3 +16,4 @@ cp $(pwd)/bin/ueventmonitor $BUILDROOT_TARGET_PATH/usr/bin/
 
 cp bin/Carmachine $BUILDROOT_TARGET_PATH/usr/bin/
 cp S50rk3399init $BUILDROOT_TARGET_PATH/etc/init.d/
+cp alsa_conf/rt5651/alsa.conf $BUILDROOT_TARGET_PATH/usr/share/alsa/alsa.conf
