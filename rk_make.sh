f3bd0ec70b6bc13ff3ff260a764dc04e7045befe
zzc <zzc@rock-chips.com>
2017-01-09 09:33:08 +0800

    add wifi firmware
    Change-Id: I915c55539eb4765d038918d985fb0724769ee5a0
    Signed-off-by: zzc <zzc@rock-chips.com>

--- rk_make.sh
+++ rk_make.sh
@@ -17,3 +17,7 @@ cp $(pwd)/bin/ueventmonitor $BUILDROOT_TARGET_PATH/usr/bin/
 cp bin/Carmachine $BUILDROOT_TARGET_PATH/usr/bin/
 cp S50rk3399init $BUILDROOT_TARGET_PATH/etc/init.d/
 cp alsa_conf/rt5651/alsa.conf $BUILDROOT_TARGET_PATH/usr/share/alsa/alsa.conf
+
+#wifi firmware
+mkdir -p $BUILDROOT_TARGET_PATH/system/etc
+cp -rf firmware $BUILDROOT_TARGET_PATH/system/etc/
