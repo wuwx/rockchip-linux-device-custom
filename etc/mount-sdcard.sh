27fa6c3cab46b7a73bcd8a0457097f26899ba8e4
hero.huang <hero.huang@rock-chips.com>
2017-08-08 10:11:25 +0800

    Compatible with exfat and vfat file system
    Change-Id: Ie00a135a812d04a38858f934069a8a8ee5ccadb8
    Signed-off-by: hero.huang <hero.huang@rock-chips.com>

--- etc/mount-sdcard.sh
+++ etc/mount-sdcard.sh
@@ -1,5 +1,15 @@
 #!/bin/sh
-mount /dev/$1 /mnt/sdcard
+
+TYPE=`blkid /dev/$1 | awk -F ';' '{print $4}'`
+
+if [ "${TYPE}" = "exfat" ] ;then
+        mount.exfat /dev/$1 /mnt/sdcard
+elif [ "${TYPE}" = "ntfs" ] ;then
+        mount.ntfs-3g /dev/$1 /mnt/sdcard
+else
+        mount /dev/$1 /mnt/sdcard
+fi
+
 echo /dev/$1 > /sys/kernel/config/usb_gadget/g1/functions/mass_storage.0/lun.0/file 
 
 sync
