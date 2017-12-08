69a403f4416f44fe3a43b38704ae7efd46b88934
Guochun Huang <hero.huang@rock-chips.com>
2017-12-08 09:14:43 +0800

    rk3399: device: add module configuration options
    Change-Id: I63e7ec3b02895050e5fd6253235cf386e0a8ea9f
    Signed-off-by: Guochun Huang <hero.huang@rock-chips.com>

---
+++ package_config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+enable_adb=yes
+enable_usb=yes
+enable_sdcard_udisk=yes
+enable_bluetooth=yes
+
+#enable build app
+enable_camera=yes
+enable_gallery=yes
+enable_music=yes
+enable_video=yes
+enable_settings=yes
+
+#select display platform
+PLATFORM_WAYLAND=no
+
