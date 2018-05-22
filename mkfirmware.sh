5b629b152b90c749e4aff89321179d5c6c477812
Eddie Cai <eddie.cai.linux@gmail.com>
2018-05-22 17:23:24 +0800

    rk3399: update mkfirmware.sh
    Change-Id: Ifee86f40fa5d00c8d21c61ba568cfec6bb61db2a
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- mkfirmware.sh
+++ mkfirmware.sh
@@ -3,26 +3,75 @@ IMAGE_OUT_PATH=$(pwd)/rockimg
 KERNEL_PATH=$(pwd)/kernel
 UBOOT_PATH=$(pwd)/u-boot
 ROOTFS_PATH=$(pwd)/rootfs
-
+DEVICE_IMG_PATH=$(pwd)/device/rockchip/rk3399/rockimg
+PARAMETER_PATH=$DEVICE_IMG_PATH/parameter.txt
+OEM_IMG_PATH=$DEVICE_IMG_PATH/oem.img
+USER_DATA_IMG_PATH=$DEVICE_IMG_PATH/userdata.img
+MISC_IMG_PATH=$DEVICE_IMG_PATH/wipe_all-misc.img
+ROOTFS_IMG_PATH=$(pwd)/buildroot/output/rockchip_rk3399/images/rootfs.ext4
+RECOVERY_PATH=$(pwd)/buildroot/output/rockchip_rk3399_recovery/images/recovery.img
 mkdir -p $IMAGE_OUT_PATH
-if [ $1 = buildroot ]
+
+if [ $1 = debian ]
 then
-    #cd buildroot && make && cd -
-    rm -rf $IMAGE_OUT_PATH
-    mkdir -p $IMAGE_OUT_PATH
-    echo "Package rootfs.img now"
-    source $(pwd)/device/rockchip/rk3399/mkrootfs.sh
-    cp $(pwd)/buildroot/output/images/rootfs.ext4 $IMAGE_OUT_PATH/rootfs.img
-elif [ $1 = debian ]
-then
-    echo "Package rootfs.img now"
-    cp $ROOTFS_PATH/linaro-rootfs.img $IMAGE_OUT_PATH/rootfs.img
+	echo -n "create rootfs.img..."
+	cp $ROOTFS_PATH/linaro-rootfs.img $IMAGE_OUT_PATH/rootfs.img
+	echo "done."
 else
-    echo -e "\e[31m error: please use the ./mkfirmware.sh debian or ./mkfirmware.sh buildroot \e[0m"
+	echo -n "create rootfs.img..."
+	cp $ROOTFS_IMG_PATH $IMAGE_OUT_PATH/rootfs.img
+	echo "done"
 fi
 
-cp $(pwd)/device/rockchip/rk3399/rockimg/parameter.txt $IMAGE_OUT_PATH/
-cp $(pwd)/device/rockchip/rk3399/rockimg/oem.img $IMAGE_OUT_PATH/
+if [ -f $RECOVERY_PATH ]
+then
+	echo -n "create recovery.img..."
+	cp $RECOVERY_PATH $IMAGE_OUT_PATH/
+	echo "done."
+else
+	echo -e "\e[31m error: $RECOVERY_PATH not found! \e[0m"
+	exit 0
+fi
+
+if [ -f $MISC_IMG_PATH ]
+then
+	echo -n "create misc.img..."
+	cp $MISC_IMG_PATH $IMAGE_OUT_PATH/
+	echo "done."
+else
+	echo -e "\e[31m error: $MISC_IMG_PATH not found! \e[0m"
+	exit 0
+fi
+
+if [ -f $PARAMETER_PATH ]
+then
+	echo -n "create parameter.txt..."
+	cp $PARAMETER_PATH $IMAGE_OUT_PATH/
+	echo "done."
+else
+	echo -e "\e[31m error: $PARAMETER_PATH not found! \e[0m"
+	exit 0
+fi
+
+if [ -f $OEM_IMG_PATH ]
+then
+	echo -n "create oem.img..."
+	cp $OEM_IMG_PATH $IMAGE_OUT_PATH/
+	echo "done."
+else
+	echo -e "\e[31m error: $OEM_IMG_PATH not found! \e[0m"
+	exit 0
+fi
+
+if [ -f $USER_DATA_IMG_PATH ]
+then
+	echo -n "create userdata.img..."
+	cp $USER_DATA_IMG_PATH $IMAGE_OUT_PATH/
+	echo "done."
+else
+	echo -e "\e[31m error: $USER_DATA_IMG_PATH not found! \e[0m"
+	exit 0
+fi
 
 if [ -f $UBOOT_PATH/uboot.img ]
 then
