2d60d28dd6b9732fe20e455b7d1e060ef0537b2d
Eddie Cai <eddie.cai.linux@gmail.com>
2018-05-29 16:14:48 +0800

    rk3399: mkfirmware.sh: use link instead of copy
    Change-Id: Ie365ab1d08eca697e6b5b1858050d7667b19c86a
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- mkfirmware.sh
+++ mkfirmware.sh
@@ -23,18 +23,18 @@ fi
 if [ $ROOTFS_TYPE = debian ]
 then
 	echo -n "create rootfs.img..."
-	cp $ROOTFS_PATH/linaro-rootfs.img $IMAGE_OUT_PATH/rootfs.img
+	ln -s $ROOTFS_PATH/linaro-rootfs.img $IMAGE_OUT_PATH/rootfs.img
 	echo "done."
 else
 	echo -n "create rootfs.img..."
-	cp $ROOTFS_IMG_PATH $IMAGE_OUT_PATH/rootfs.img
+	ln -s $ROOTFS_IMG_PATH $IMAGE_OUT_PATH/rootfs.img
 	echo "done"
 fi
 
 if [ -f $RECOVERY_PATH ]
 then
 	echo -n "create recovery.img..."
-	cp $RECOVERY_PATH $IMAGE_OUT_PATH/
+	ln -s $RECOVERY_PATH $IMAGE_OUT_PATH/
 	echo "done."
 else
 	echo -e "\e[31m error: $RECOVERY_PATH not found! \e[0m"
@@ -44,7 +44,7 @@ fi
 if [ -f $MISC_IMG_PATH ]
 then
 	echo -n "create misc.img..."
-	cp $MISC_IMG_PATH $IMAGE_OUT_PATH/misc.img
+	ln -s $MISC_IMG_PATH $IMAGE_OUT_PATH/misc.img
 	echo "done."
 else
 	echo -e "\e[31m error: $MISC_IMG_PATH not found! \e[0m"
@@ -54,7 +54,7 @@ fi
 if [ -f $PARAMETER_PATH ]
 then
 	echo -n "create parameter.txt..."
-	cp $PARAMETER_PATH $IMAGE_OUT_PATH/
+	ln -s $PARAMETER_PATH $IMAGE_OUT_PATH/
 	echo "done."
 else
 	echo -e "\e[31m error: $PARAMETER_PATH not found! \e[0m"
@@ -64,7 +64,7 @@ fi
 if [ -f $OEM_IMG_PATH ]
 then
 	echo -n "create oem.img..."
-	cp $OEM_IMG_PATH $IMAGE_OUT_PATH/
+	ln -s $OEM_IMG_PATH $IMAGE_OUT_PATH/
 	echo "done."
 else
 	echo -e "\e[31m error: $OEM_IMG_PATH not found! \e[0m"
@@ -74,7 +74,7 @@ fi
 if [ -f $USER_DATA_IMG_PATH ]
 then
 	echo -n "create userdata.img..."
-	cp $USER_DATA_IMG_PATH $IMAGE_OUT_PATH/
+	ln -s $USER_DATA_IMG_PATH $IMAGE_OUT_PATH/
 	echo "done."
 else
 	echo -e "\e[31m error: $USER_DATA_IMG_PATH not found! \e[0m"
@@ -84,7 +84,7 @@ fi
 if [ -f $UBOOT_PATH/uboot.img ]
 then
         echo -n "create uboot.img..."
-        cp -a $UBOOT_PATH/uboot.img $IMAGE_OUT_PATH/uboot.img
+        ln -s $UBOOT_PATH/uboot.img $IMAGE_OUT_PATH/uboot.img
         echo "done."
 else
         echo -e "\e[31m error: $UBOOT_PATH/uboot.img not found! Please make it from $UBOOT_PATH first! \e[0m"
@@ -94,7 +94,7 @@ fi
 if [ -f $UBOOT_PATH/trust.img ]
 then
         echo -n "create trust.img..."
-        cp -a $UBOOT_PATH/trust.img $IMAGE_OUT_PATH/trust.img
+        ln -s $UBOOT_PATH/trust.img $IMAGE_OUT_PATH/trust.img
         echo "done."
 else
         echo -e "\e[31m error: $UBOOT_PATH/trust.img not found! Please make it from $UBOOT_PATH first! \e[0m"
@@ -104,7 +104,7 @@ fi
 if [ -f $UBOOT_PATH/*_loader_*.bin ]
 then
         echo -n "create loader..."
-        cp -a $UBOOT_PATH/*_loader_*.bin $IMAGE_OUT_PATH/MiniLoaderAll.bin
+        ln -s $UBOOT_PATH/*_loader_*.bin $IMAGE_OUT_PATH/MiniLoaderAll.bin
         echo "done."
 else
 	echo -e "\e[31m error: $UBOOT_PATH/*loader_*.bin not found,or there are multiple loaders! Please make it from $UBOOT_PATH first! \e[0m"
@@ -115,7 +115,7 @@ fi
 if [ -f $KERNEL_PATH/boot.img ]
 then
 	echo -n "create boot.img..."
-	cp -a $KERNEL_PATH/boot.img $IMAGE_OUT_PATH/boot.img
+	ln -s $KERNEL_PATH/boot.img $IMAGE_OUT_PATH/boot.img
 	echo "done."
 else
 	echo -e "\e[31m error: $KERNEL_PATH/boot.img not found! \e[0m"
