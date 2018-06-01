2500453719ce59418249d66f639ad069ff894f8a
Eddie Cai <eddie.cai.linux@gmail.com>
2018-06-01 17:08:20 +0800

    build_all.sh: add mk-uboot.sh script
    Change-Id: I5072f2268abac2ae2952ed9f5181479cd3df79a9
    Signed-off-by: Eddie Cai <eddie.cai.linux@gmail.com>

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,6 @@
 #! /bin/bash
 
-TOP_DIR=$(pwd)
-BUILDROOT_PATH=$(pwd)/buildroot/
-PRODUCT_PATH=$(pwd)/device/rockchip/rk3288
-
-
-cd u-boot && ./make.sh fennec-rk3288 && cd -
+./mk-uboot.sh
 ./mk-kernel.sh
 ./mk-rootfs.sh
 ./mk-recovery.sh
