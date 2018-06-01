398be6b1494aeae3348b9fc1deecfee0f3a0271f
Caesar Wang <wxt@rock-chips.com>
2018-06-01 10:26:26 +0800

    rk3399: add ./mk-uboot.sh for linux
    Change-Id: Ie70ac22a1f25cf8ec49df96759f5af51779716c3
    Signed-off-by: Caesar Wang <wxt@rock-chips.com>

--- build_all.sh
+++ build_all.sh
@@ -1,11 +1,6 @@
 #! /bin/bash
 
-TOP_DIR=$(pwd)
-BUILDROOT_PATH=$(pwd)/buildroot/
-PRODUCT_PATH=$(pwd)/device/rockchip/rk3399
-
-
-cd u-boot && ./make.sh evb-rk3399 && cd -
+./mk-uboot.sh
 ./mk-kernel.sh
 ./mk-rootfs.sh
 ./mk-recovery.sh
